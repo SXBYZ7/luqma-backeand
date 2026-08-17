7a8112998c0cce0b4bc3d930c8e81f304afb7356
SXBYZ7 <y56689337@gmail.com>
2026-08-18 02:41:45 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,13 +5,17 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-  res.send("LUQMA ROOT WORKING");
+  res.send("LUQMA ROOT V5");
 });
 
-app.get("/abc123", (req, res) => {
-  res.send("LUQMA ROUTE WORKING");
+app.get("/health", (req, res) => {
+  res.send("HEALTH WORKING V5");
+});
+
+app.get("/test123", (req, res) => {
+  res.send("TEST123 WORKING V5");
 });
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log("LUQMA RUNNING ON PORT " + PORT);
+  console.log("LUQMA V5 RUNNING ON PORT " + PORT);
 });
